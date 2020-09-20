7e6484fa3dc17934e44926bed902e163eac7469d
baljit <baljit@trint.com>
2020-09-20 19:28:57 +0100

    inital error test in place

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { Home } from './Home';
 
 describe(':Home tests', () => {
@@ -7,8 +7,29 @@ describe(':Home tests', () => {
     it('should display content to help user search for businesses', () => {
       const { getByText } = render(<Home />);
 
-      expect(getByText('Please enter the name of the business you are searching for and click search to view results')).toBeDefined();
+      expect(
+        getByText(
+          'Please enter the name of the business you are searching for and click search to view results'
+        )
+      ).toBeDefined();
       expect(getByText('Search')).toBeDefined();
+    });
+  });
+
+  describe(':Validation', () => {
+    describe(':bad data', () => {
+      it('Should display a message when searching with no text', () => {
+        const { getByText } = render(<Home />);
+
+        const searchButton = getByText('Search');
+        fireEvent.click(searchButton);
+
+        const errorText = getByText(
+          /Please enter a business name before clicking search/
+        );
+
+        expect(errorText).toBeInTheDocument();
+      });
     });
   });
 });
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,26 @@
+import React, { useState } from 'react';
 import { RouteComponentProps } from '@reach/router';
-import React from 'react';
 import { Button, Grid, TextField, Typography } from '@material-ui/core';
-import './Home.css'
+import './Home.css';
 
 export const Home: React.FC<RouteComponentProps> = () => {
+  const [searchText, setSearchText] = useState<string>('');
+  const [searchErrorMessage, setSearchErrorMessage] = useState<string>('');
+
+  const handleSearchSubmit = () => {
+    setSearchErrorMessage('');
+    if (searchText.length === 0) {
+      setSearchErrorMessage(
+        'Please enter a business name before clicking search'
+      );
+    }
+  };
+
+  const handleSearchOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchErrorMessage('');
+    setSearchText(e.target.value);
+  };
+
   return (
     <div className='App-Home'>
       <Grid container spacing={1} direction='row'>
@@ -26,13 +43,22 @@ export const Home: React.FC<RouteComponentProps> = () => {
               id='business-name-search'
               label='Business Name'
               variant='outlined'
+              onChange={handleSearchOnChange}
+              value={searchText}
+              error={searchErrorMessage.length > 0}
+              helperText={searchErrorMessage}
               fullWidth
             />
           </Grid>
         </Grid>
         <Grid item xs={1}>
           <Grid container justify='flex-end' alignItems='flex-start'>
-            <Button size='large' variant='contained' color='primary'>
+            <Button
+              size='large'
+              variant='contained'
+              color='primary'
+              onClick={handleSearchSubmit}
+            >
               Search
             </Button>
           </Grid>
